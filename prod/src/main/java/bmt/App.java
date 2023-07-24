eb4cc2ba38ce43029f2f2d604c767c6c3aa7be69
Isha Choudhary <ishachoudhary521@gmail.com>
2023-07-24 14:59:26 +0530

    file changed

--- prod/src/main/java/bmt/App.java
+++ prod/src/main/java/bmt/App.java
@@ -9,7 +9,7 @@ public class App
   public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-          System.out.println( "Hellotech!" );
+        System.out.println( "this is dev environment" );
 
     }  
 }
